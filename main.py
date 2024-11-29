4bed6a780810b4abe5e6d89b796cce3aa6052a54
Sergio Rendon <checho1107@gmail.com>
2024-11-29 00:10:09 +0000

    carga del archivo main

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+
+app = FastAPI()
+
+# Cargar el modelo
+model_pipeline = joblib.load('model_pipeline.joblib')
+
+class SalaryPredictionInput(BaseModel):
+    work_year: int
+    experience_level: str
+    employment_type: str
+    job_title: str
+    employee_residence: str
+    company_location: str
+    company_size: str
+
+@app.post("/predict")
+def predict_salary(input_data: SalaryPredictionInput):
+    input_dict = input_data.dict()
+    input_df = pd.DataFrame([input_dict])
+    prediction = model_pipeline.predict(input_df)[0]
+    return {"predicted_salary": prediction}
